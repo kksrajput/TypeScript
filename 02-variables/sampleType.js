0b8e8ae399ff56b50ac6dc3eba4d7610fdedd0ab
kksrajput <kaushalkantsingh@gmail.com>
2022-11-02 01:32:42 +0530

    typescript_basics

---
+++ 02-variables/sampleType.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 90;
+var firstName = "Ash";
+var lastName = "Singh";
+console.log("found");
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sampleType.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 90;
+let firstName : string = "Ash";
+let lastName : string = "Singh";
+
+console.log("found");
+console.log("The grade is "+ grade);
+console.log("Hi "+firstName+" "+lastName);
+
+
+console.log(`Hi ${firstName} ${lastName}`);
