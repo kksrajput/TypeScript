e70b6876e4dda0b80b771c01f1479773f0b89d11
kksrajput <kaushalkantsingh@gmail.com>
2022-11-03 23:00:16 +0530

    classes-1

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,10 @@
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+var myCustomer = new A();
+myCustomer.firstName = "Ash";
+myCustomer.lastName = "Singh";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,11 @@
+class A{
+    firstName: string;
+    lastName: string;
+}
+
+let myCustomer = new A();
+
+myCustomer.firstName = "Ash";
+myCustomer.lastName = "Singh";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
